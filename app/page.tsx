049d99df5477311404c03b68126175deabf34873
Sharjun Hussain <sharjoonibnuhussain99@gmail.com>
2025-04-09 19:08:03 +0530

    fix:layout issue client in hero component window object is not called on client copmonnet like useeffect or usestate and now fixed by confitional rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
+"use client";
 import { Fragment } from "react";
-
-import { FollowerPointerCard } from "./Component/ui/Following-pointer";
-import AboutSection from "./Sections/About";
 import { Timeline } from "./Component/ui/timeline";
 import { experience } from "./lib/Data";
 import ProjectsSection from "./Sections/AchivementCounter";
 import Hero from "./Sections/Hero";
 import SkillsSection from "./Sections/Skills";
-import { ProjectsSections } from "./Sections/ProjectShowcase";
-import { ContactGlobe } from "./Sections/FooterComponents/ContactGlobe";
-import { AIContact } from "./Sections/FooterComponents/AICOntact";
-import { VideoContactCard } from "./Sections/FooterComponents/VideoBusinessCard";
-import { SecureContact } from "./Sections/FooterComponents/SecureCOntact";
-import { ARContactCard } from "./Sections/FooterComponents/ARContactCard";
 import ContactSection from "./Sections/Contact";
 
 export default function Home() {
